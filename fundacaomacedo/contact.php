201b76d410432688719b8c6646df1d7d38561fbb
Micael josé de Lima <micaeljose144@gmail.com>
2023-11-17 18:45:46 -0300

    formulario php

--- fundacaomacedo/contact.php
+++ fundacaomacedo/contact.php
@@ -1,41 +1,18 @@
-<!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="pt-br"> <!--<![endif]-->
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <title>Fundação Macedo/ Formulário</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="description" content="Fundação Macedo - Charity, Fundraising & Non-Profit HTML5 Template">
-    <meta name="author" content="Fundação Macedo">
-    <link rel="shortcut icon" href="images/logo1.png " />
+    if(isset($_POST['submit']))
+    {
 
-    <!-- Correção HTML5, para suporte do IE6-8 a elementos HTML5 -->
-    <!--[se for IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-          <![endif]-->
+    include_once(contact_email.php);
 
-    <!-- Os estilos-->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
-    <link href="css/animate.css" rel="stylesheet" type="text/css">
-    <link href="css/venobox.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="css/styles.css" />
-</head>
-<body>
-    <header> 
-        <h1> Resultado do Processamento <h1>
-    </header>
-    <main>
-        <?php 
-        $nome = $_GET["Nome"]
-        echo "<p> É um prazer e conhecer, <strong>$n $s! Este é o meu site!";
+    $nome = $_POST['user_name'];
+    $telefone = $_POST['user_phone'];
+    $data_nasc = $_POST['user_birthdate'];
+    $cidade = $_POST['user_city'];
 
-    ?>
-    <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    $result = mysqli_query($conexao, "INSERT INTO clientes(nome,cidade,telefone,data_nasc)
+    VALUES ('$nome','$telefone','$data_nasc','$cidade')");
 
-    </main>
-</body>
-</html>
+    }
+
+?>
--- fundacaomacedo/contact_email.php
+++ fundacaomacedo/contact_email.php
@@ -1,44 +1,19 @@
 <?php
 
-if (!isset($_REQUEST['safety_key'])) {
-    die();
+$dbHost = 'Localhost'
+$dbUsername = 'root'
+$dbPassword = '';
+$dbName = 'clientes';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+if($conexao->connect_erro)
+{
+    echo "Erro";
 }
-// Email do Administrador.
+else
+{
+    echo "Conexão efetuada com sucesso";
+}
 
-$para = "fundacaomacedope@gmail.com"; // insira o endereço de e-mail aqui.
-
-// Obtendo valores da URL.
-
-$nome_usuario = $_POST['user_name'];
-
-$email_usuario = $_POST['user_email'];
-
-$assunto_email = $_POST['email_subject'];
-
-$mensagem_email = $_POST['email_message'];
-
-$template = '<div style="padding:50px; color:#2C2C2C;">Olá ' . $nome_usuario . ',<br/>'
-        . '<br/>Obrigado...! Por entrar em contato conosco.<br/><br/>'
-        . 'Nome:' . $nome_usuario . '<br/>'
-        . 'E-mail:' . $email_usuario . '<br/>'
-        . 'Mensagem:' . $mensagem_email . '<br/><br/>'
-        . 'Esta é uma confirmação de contato.'
-        . '<br/>'
-        . 'Entraremos em contato assim que possível.</div>';
-
-$mensagem = "<div style=\"background-color:#FAFAFA; color:#2C2C2C;\">" . $template . "</div>";
-
-$cabecalhos = 'MIME-Version: 1.0' . "\r\n";
-
-$cabecalhos .= 'Content-type: text/html; charset=utf-8; charset=iso-8859-1' . "\r\n";
-
-$cabecalhos .= 'De:' . $email_usuario . "\r\n"; // E-mail do remetente
-
-mail($para, $assunto_email, $mensagem, $cabecalhos, '');
-
-$dados = array(
-    'status' => 1,
-    'msg' => "Recebemos sua consulta. Entraremos em contato em breve."
-);
-
-echo json_encode($dados);
+?>
