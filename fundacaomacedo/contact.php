ba2f076d294d445e15b5c7c70506e766df4cd6df
Micael josé de Lima <micaeljose144@gmail.com>
2023-11-16 22:10:49 -0300

    Inicio de um sonho/ Deu tudo errado

---
+++ fundacaomacedo/contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="pt-br"> <!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <title>Fundação Macedo/ Formulário</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="Fundação Macedo - Charity, Fundraising & Non-Profit HTML5 Template">
+    <meta name="author" content="Fundação Macedo">
+    <link rel="shortcut icon" href="images/logo1.png " />
+
+    <!-- Correção HTML5, para suporte do IE6-8 a elementos HTML5 -->
+    <!--[se for IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+          <![endif]-->
+
+    <!-- Os estilos-->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/venobox.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/styles.css" />
+</head>
+<body>
+    <header> 
+        <h1> Resultado do Processamento <h1>
+    </header>
+    <main>
+        <?php 
+        $nome = $_GET["Nome"]
+        echo "<p> É um prazer e conhecer, <strong>$n $s! Este é o meu site!";
+
+    ?>
+    <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+
+    </main>
+</body>
+</html>
